de588f0e7c9f09218481f01e3e5549fe6fb95159
KayTee1 <truong.kha@outlook.com>
2023-08-14 18:02:55 +0300

    starting problem 2

---
+++ add_two_numbers/add_two_numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+
+
+int main() {
+    std::vector<int> l1_values = {2,4,3};
+    std::vector<int> l2_values = {5,6,4};
+
+
+    return 0;
+}
