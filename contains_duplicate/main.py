b34ae4dbee394a6c403345282ccc079b4df194a1
KayTee1 <truong.kha@outlook.com>
2023-09-11 17:43:52 +0300

    finished contains duplicate

---
+++ contains_duplicate/main.py
@@ -0,0 +1,15 @@
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+
+        nums.sort()
+
+        for i in range(1, len(nums)):
+            if(nums[i] == nums[i - 1]):
+                return True
+
+        return False
+
+nums = [1,2,3,1]
+
+solu = Solution()
+print(solu.containsDuplicate(nums))
