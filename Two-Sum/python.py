4ff690cd42b5eef98f77ff49ea5a3a9b84ec3f07
KayTee1 <truong.kha@outlook.com>
2023-09-11 19:37:53 +0300

    finished valid_anagram, learing hashmaps

--- Two-Sum/python.py
+++ Two-Sum/python.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         hashmap = {}
-
         for i, num in enumerate(nums):
             diff = target - num
+
             if diff in hashmap:
                 return [hashmap[diff], i]
             hashmap[num] = i
