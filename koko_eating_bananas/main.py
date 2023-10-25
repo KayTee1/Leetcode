8cc8a076ef6af63659f75cea5a3e79f093b2def5
KayTee1 <kha.truong@tuni.fi>
2023-10-25 17:02:09 +0300

    did binarysrch

---
+++ koko_eating_bananas/main.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+
+        lo, hi = 1, max(piles)
+        while lo <= hi:
+            mid = lo+(hi-lo)//2
+            count = 0
+            for i in piles:
+                count += i//mid
+                if i % mid != 0:
+                    count += 1
+            if count > h:
+                lo = mid+1
+            else:
+                hi = mid - 1
+        return lo
