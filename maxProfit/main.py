132312579a0d9d1297e6ea6a48f76df95b0ff2c1
KayTee1 <kha.truong@tuni.fi>
2023-10-29 22:01:09 +0200

    did besttimetobuyandsellstock

--- maxProfit/main.py
+++ maxProfit/main.py
@@ -1,9 +1,19 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
+        l, r = 0, 1
+        max_profit = 0
+
+        while r < len(prices):
+            if prices[r] < prices[l]:
+                l = r
+            else:
+                currProfit = prices[r] - prices[l]
+                max_profit = max(max_profit, currProfit)
+            r += 1
+
+        return max_profit
 
 
-
-
-prices = [7,1,5,3,6,4]
+prices = [7, 1, 5, 3, 6, 4]
 solu = Solution()
 print(solu.maxProfit(prices))
